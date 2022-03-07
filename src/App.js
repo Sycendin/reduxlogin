e89c011381fff3c9e1e6a93d4bd1cbbe5467101f
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-03-07 00:00:24 -0500

    adding basic particle background

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import "./App.css";
 import Counter from "./components/Counter";
 import SignIn from "./components/Signin/Sigin";
 import Register from "./components/Register/Register";
+import { Fragment } from "react";
+import Background from "./background/background";
 
 function App() {
   // const loggedIn = useSelector((state) => state.isLogged);
-  return <Counter></Counter>;
+  return (
+    <Fragment>
+      <div className="App">
+        <Background></Background>
+        <Counter></Counter>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;

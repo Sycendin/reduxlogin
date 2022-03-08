0479a337b312103a2be7b98808a9fc1e8bd0d2e5
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-03-08 01:36:24 -0500

    adding updated register componenent where state switches on inpt and makes call to server on submit

--- src/reducers/allReducers.js
+++ src/reducers/allReducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { counterReducerP1 } from "./counter";
 import { routeSelect } from "./route";
+import { user } from "./user";
 
 const allReducers = combineReducers({
   counterP1: counterReducerP1,
   routeSelect: routeSelect,
+  user: user,
 });
 export default allReducers;
